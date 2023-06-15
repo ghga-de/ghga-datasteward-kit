5bd0370e63f70a9c8bb743213838f9a273fe057d
Léon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2023-06-15 11:52:41 +0000

    Begin happy test for metadata operations

---
+++ tests/fixtures/metadata.py
@@ -0,0 +1,22 @@
+# Copyright 2023 Universität Tübingen, DKFZ and EMBL
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Metadata paths for tests"""
+
+from tests.fixtures.utils import BASE_DIR
+
+METADATA_CONFIG_PATH = BASE_DIR / "metadata_config.yaml"
+ORIGINAL_METADATA_PATH = BASE_DIR / "original_metadata.yaml"
+ORIGINAL_MODEL_PATH = BASE_DIR / "original_model.yaml"
