8e1c7869ef5f92e2d97cb57cadc24d42bd28239e
Léon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2023-11-08 08:00:59 +0000

    Apply microservice repository template

--- scripts/update_config_docs.py
+++ scripts/update_config_docs.py
@@ -19,67 +19,102 @@
 example config yaml (or check whether these files are up to date).
 """
 
+import importlib
 import json
+import subprocess
 import sys
+from difflib import unified_diff
 from pathlib import Path
+from typing import Any
 
-import jsonschema2md
-from pydantic import BaseSettings
+import yaml
+
 from script_utils.cli import echo_failure, echo_success, run
-
-from ghga_datasteward_kit.config import CONFIG_CLASSES
 
 HERE = Path(__file__).parent.resolve()
 REPO_ROOT_DIR = HERE.parent
+DEV_CONFIG_YAML = REPO_ROOT_DIR / ".devcontainer" / ".dev_config.yaml"
+GET_PACKAGE_NAME_SCRIPT = HERE / "get_package_name.py"
+EXAMPLE_CONFIG_YAML = REPO_ROOT_DIR / "example_config.yaml"
+CONFIG_SCHEMA_JSON = REPO_ROOT_DIR / "config_schema.json"
 
 
 class ValidationError(RuntimeError):
     """Raised when validation of config documentation fails."""
 
 
-def get_schema(config_cls: type[BaseSettings]) -> dict:
+def get_config_class():
+    """
+    Dynamically imports and returns the Config class from the current service.
+    This makes the script service repo agnostic.
+    """
+    # get the name of the microservice package
+    with subprocess.Popen(
+        args=[GET_PACKAGE_NAME_SCRIPT],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    ) as process:
+        assert (
+            process.wait() == 0 and process.stdout is not None
+        ), "Failed to get package name."
+        package_name = process.stdout.read().decode("utf-8").strip("\n")
+
+    # import the Config class from the microservice package:
+    config_module: Any = importlib.import_module(f"{package_name}.config")
+    config_class = config_module.Config
+
+    return config_class
+
+
+def get_dev_config():
+    """Get dev config object."""
+    config_class = get_config_class()
+    return config_class(config_yaml=DEV_CONFIG_YAML)
+
+
+def get_schema() -> str:
     """Returns a JSON schema generated from a Config class."""
 
-    return json.loads(config_cls.schema_json(indent=2))
+    config = get_dev_config()
+    return config.schema_json(indent=2)  # change eventually to .model_json_schema(...)
 
 
-def generate_config_docs(config_cls: type[BaseSettings]) -> str:
-    """Generate markdown-formatted documentation for the configration parameters
-    listed in the config schema."""
+def get_example() -> str:
+    """Returns an example config YAML."""
 
-    config_schema = get_schema(config_cls)
-
-    parser = jsonschema2md.Parser(
-        examples_as_yaml=False,
-        show_examples="all",
+    config = get_dev_config()
+    normalized_config_dict = json.loads(
+        config.json()  # change eventually to .model_dump_json()
     )
-    md_lines = parser.parse_schema(config_schema)
-
-    return "\n".join(md_lines)
+    return yaml.dump(normalized_config_dict)  # pyright: ignore
 
 
-def get_docs_file_path(config_type: str) -> Path:
-    """Returns the path to the file containing the documentation for the config
-    options."""
-
-    return REPO_ROOT_DIR / f"{config_type}_config.md"
-
-
-def update_docs(config_type: str):
+def update_docs():
     """Update the example config and config schema files documenting the config
     options."""
 
-    config_cls = CONFIG_CLASSES[config_type]
+    example = get_example()
+    with open(EXAMPLE_CONFIG_YAML, "w", encoding="utf-8") as example_file:
+        example_file.write(example)
 
-    docs = generate_config_docs(config_cls)
-
-    with open(
-        get_docs_file_path(config_type=config_type), "w", encoding="utf-8"
-    ) as schema_file:
-        schema_file.write(docs)
+    schema = get_schema()
+    with open(CONFIG_SCHEMA_JSON, "w", encoding="utf-8") as schema_file:
+        schema_file.write(schema)
 
 
-def check_docs(config_type: str):
+def print_diff(expected: str, observed: str):
+    """Print differences between expected and observed files."""
+    echo_failure("Differences in Config YAML:")
+    for line in unified_diff(
+        expected.splitlines(keepends=True),
+        observed.splitlines(keepends=True),
+        fromfile="expected",
+        tofile="observed",
+    ):
+        print("   ", line.rstrip())
+
+
+def check_docs():
     """Check whether the example config and config schema files documenting the config
     options are up to date.
 
@@ -87,15 +122,22 @@ def check_docs(config_type: str):
         ValidationError: if not up to date.
     """
 
-    config_cls = CONFIG_CLASSES[config_type]
+    example_expected = get_example()
+    with open(EXAMPLE_CONFIG_YAML, encoding="utf-8") as example_file:
+        example_observed = example_file.read()
+    if example_expected != example_observed:
+        print_diff(example_expected, example_observed)
+        raise ValidationError(
+            f"Example config YAML at '{EXAMPLE_CONFIG_YAML}' is not up to date."
+        )
 
-    expected_docs = generate_config_docs(config_cls)
-    with open(
-        get_docs_file_path(config_type=config_type), "r", encoding="utf-8"
-    ) as schema_file:
-        observed_docs = schema_file.read()
-    if expected_docs != observed_docs:
-        raise ValidationError(f"Docs for config '{config_type}' are not up to date.")
+    schema_expected = get_schema()
+    with open(CONFIG_SCHEMA_JSON, encoding="utf-8") as schema_file:
+        schema_observed = schema_file.read()
+    if schema_expected != schema_observed:
+        raise ValidationError(
+            f"Config schema JSON at '{CONFIG_SCHEMA_JSON}' is not up to date."
+        )
 
 
 def main(check: bool = False):
@@ -103,16 +145,14 @@ def main(check: bool = False):
 
     if check:
         try:
-            for config_type in CONFIG_CLASSES:
-                check_docs(config_type=config_type)
+            check_docs()
         except ValidationError as error:
             echo_failure(f"Validation failed: {error}")
             sys.exit(1)
         echo_success("Config docs are up to date.")
         return
 
-    for config_type in CONFIG_CLASSES:
-        update_docs(config_type=config_type)
+    update_docs()
     echo_success("Successfully updated the config docs.")
 
 
