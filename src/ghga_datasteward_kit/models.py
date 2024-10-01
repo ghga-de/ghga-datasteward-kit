52679b1260c21a5ae6928451034a7ddf817b7b79
Thomas Zajac <thomas-jakob.zajac@uni-tuebingen.de>
2024-10-01 14:25:30 +0200

    Update src/ghga_datasteward_kit/models.py
    Co-authored-by: Byron Himes <byro93@live.com>

--- src/ghga_datasteward_kit/models.py
+++ src/ghga_datasteward_kit/models.py
@@ -148,7 +148,7 @@ class OutputMetadata(Metadata):
             bucket_id = data["Bucket ID"]
         except KeyError:
             LOG.warning(
-                "Could not find bucket ID in metadata, populating with configured alias '%s' instead.",
+                "Could not find bucket ID in metadata, populating with configured bucket '%s' instead.",
                 selected_bucket,
             )
             bucket_id = selected_bucket
